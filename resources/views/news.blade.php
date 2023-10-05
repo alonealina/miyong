56e2648bc9dd8e7041ef00bbe721b0432306fe50
akamzrk <mizerk87@gmail.com>
2023-10-05 14:08:55 +0900

    newsのpagenation修正

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -61,21 +61,15 @@
                 <div class="sec8_news_title">textextextextextextextextextextextext</div>
             </a>
         </div>
-        <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-center">
-                <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-                </a>
-                </li>
-                <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-                </a>
-                </li>
+        <nav class="page_nav" aria-label="Page navigation">
+            <ul class="pagenation justify-content-center">
+                <a class="" href="#"><li class="page_active">1</li></a>
+                <li class="page"><a class="-" href="#">2</a></li>
+                <li class="page"><a class="-" href="#">3</a></li>
+                <li class="page"><a class="-" href="#">4</a></li>
+                <li class="page"><a class="-" href="#">5</a></li>
+                <li class="page dummy">...</li>
+                <a href="#" aria-label="Next"><li class="page"><span aria-hidden="true">&rsaquo;</span></li></a>
             </ul>
         </nav>
     </div>
@@ -93,7 +87,82 @@
 
 @section('content_sp')
 
-<img src="{{ asset('img/miyong_main.png') }}" class="main_img_sp" id="main_img">
+<div class=" wrapper_dotbg">
+
+<div class="page_title">お知らせ</div>
+
+<section id="section_1" class="news_section1">
+    <div class="news_container">
+        <div class="sec8_news_list">
+            <a href="{{ route('article') }}" class="sec8_news">
+                <div class="sec8_news_date">2023/04/01</div>
+                <div class="sec8_news_genre">一般</div>
+                <div class="sec8_news_title">textextextextextextextextextextextext</div>
+            </a>
+            <a href="{{ route('article') }}" class="sec8_news">
+                <div class="sec8_news_date">2023/04/01</div>
+                <div class="sec8_news_genre">一般</div>
+                <div class="sec8_news_title">textextextextextextextextextextextext</div>
+            </a>
+            <a href="{{ route('article') }}" class="sec8_news">
+                <div class="sec8_news_date">2023/04/01</div>
+                <div class="sec8_news_genre">一般</div>
+                <div class="sec8_news_title">textextextextextextextextextextextext</div>
+            </a>
+            <a href="{{ route('article') }}" class="sec8_news">
+                <div class="sec8_news_date">2023/04/01</div>
+                <div class="sec8_news_genre">一般</div>
+                <div class="sec8_news_title">textextextextextextextextextextextext</div>
+            </a>
+            <a href="{{ route('article') }}" class="sec8_news">
+                <div class="sec8_news_date">2023/04/01</div>
+                <div class="sec8_news_genre">一般</div>
+                <div class="sec8_news_title">textextextextextextextextextextextext</div>
+            </a>
+            <a href="{{ route('article') }}" class="sec8_news">
+                <div class="sec8_news_date">2023/04/01</div>
+                <div class="sec8_news_genre">一般</div>
+                <div class="sec8_news_title">textextextextextextextextextextextext</div>
+            </a>
+            <a href="{{ route('article') }}" class="sec8_news">
+                <div class="sec8_news_date">2023/04/01</div>
+                <div class="sec8_news_genre">一般</div>
+                <div class="sec8_news_title">textextextextextextextextextextextext</div>
+            </a>
+            <a href="{{ route('article') }}" class="sec8_news">
+                <div class="sec8_news_date">2023/04/01</div>
+                <div class="sec8_news_genre">一般</div>
+                <div class="sec8_news_title">textextextextextextextextextextextext</div>
+            </a>
+            <a href="{{ route('article') }}" class="sec8_news">
+                <div class="sec8_news_date">2023/04/01</div>
+                <div class="sec8_news_genre">一般</div>
+                <div class="sec8_news_title">textextextextextextextextextextextext</div>
+            </a>
+            <a href="{{ route('article') }}" class="sec8_news">
+                <div class="sec8_news_date">2023/04/01</div>
+                <div class="sec8_news_genre">一般</div>
+                <div class="sec8_news_title">textextextextextextextextextextextext</div>
+            </a>
+        </div>
+        <nav class="page_nav" aria-label="Page navigation">
+            <ul class="pagenation justify-content-center">
+                <a class="" href="#"><li class="page_active">1</li></a>
+                <li class="page"><a class="-" href="#">2</a></li>
+                <li class="page"><a class="-" href="#">3</a></li>
+                <li class="page"><a class="-" href="#">4</a></li>
+                <li class="page"><a class="-" href="#">5</a></li>
+                <li class="page dummy">...</li>
+                <a href="#" aria-label="Next"><li class="page"><span aria-hidden="true">&rsaquo;</span></li></a>
+            </ul>
+        </nav>
+    </div>
+</section>
+
+</div>
+
+<p class="footer">Copyright（C）Beauty Support All rights reserved.</p>
+
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="{{ asset('js/index_sp.js') }}"></script>
 @endsection
