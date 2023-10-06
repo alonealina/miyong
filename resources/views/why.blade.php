17c2ba347aa10cc01c01c04b0ab7e84f27c2c680
akamzrk <mizerk87@gmail.com>
2023-10-06 12:53:12 +0900

    why修正

--- resources/views/why.blade.php
+++ resources/views/why.blade.php
@@ -44,8 +44,8 @@
             韓国の医療チームが選ばれるほど、韓国の美容整形技術は<br>
             国際的にマニュア ル化されています。<br>
             中でも、骨を削らずシャープなフェイスラインを作る<br>
-            「キツネVライン形成」は、<b>世界最多の手術件数</b>を記録するほど、<br>
-            断トツの技術力を誇ります。<br>
+            「キツネVライン形成」は、<b>世界最多の手術件数</b><br>
+            を記録するほど、断トツの技術力を誇ります。<br>
             他にも、手術室の 監視カメラ設置や手術室の実名制度<br>
             (担当医師の情報を伝える)の導入など、<br>
             患者が <b>信頼できる 医療環境</b> を整えると同時に、<br>
@@ -55,7 +55,22 @@
     </div>
 </section>
 
-@include('item.social_link')
+<div class="sociallink">
+
+<section id="section_5" class="top_section_5">
+    ご相談は <span class="sec5_span">24時間</span> いつでも受付中!!<br>
+    LINEやInstagramから気軽にお問合せ
+    <div class="sec5_icon_list">
+        <a href="#!" style=""><img src="http://localhost:8000/img/line.png" class="sec5_icon"></a>
+        <a href="#!" style="padding-top: 12vw;"><img src="http://localhost:8000/img/insta.png" class="sec5_icon"></a>
+        <a href="#!" style=""><img src="http://localhost:8000/img/kakao.png" class="sec5_icon"></a>
+        <a href="#!" style="padding-top: 12vw;"><img src="http://localhost:8000/img/twitter.png" class="sec5_icon"></a>
+    </div>
+</section>
+
+<p class="footer">Copyright（C）Beauty Support All rights reserved.</p>
+
+</div>
 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="{{ asset('js/index_sp.js') }}"></script>
